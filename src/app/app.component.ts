83ff6c751a6f9d6760d33300dc00ce689f46f215
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2021-07-24 17:55:46 +0530

    Added to git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'swingcalculator';
+  stockPrice: string = '';
+  investedMoney: string = '';
+  stockPriceExpected: string = '';
+  currentStockPrice: number = 0;
+  nShares: number = 0;
+  profit: number = 0;
+  change: number = 0;
+
+  getNumberOfShares(): string {
+    console.log(this.stockPrice);
+    if (parseFloat(this.stockPrice) && this.investedMoney) {
+      this.nShares =
+        parseFloat(this.investedMoney) / parseFloat(this.stockPrice);
+      return this.nShares.toFixed(1);
+    }
+    return '0';
+  }
+
+  getExpectedValue(): string {
+    if (this.nShares) {
+      this.currentStockPrice =
+        this.nShares * parseFloat(this.stockPriceExpected);
+      console.log(this.currentStockPrice);
+      return this.currentStockPrice.toFixed(2);
+    }
+    return this.stockPriceExpected;
+  }
+
+  calculateProfit(): string {
+    if (this.nShares) {
+      this.profit = this.currentStockPrice - parseFloat(this.investedMoney);
+      return this.profit.toFixed(2);
+    }
+    return this.profit.toString();
+  }
+
+  getStockChanged(): string {
+    if (this.nShares) {
+      this.change =
+        parseFloat(this.stockPriceExpected) - parseFloat(this.stockPrice);
+      return this.change.toFixed(2);
+    }
+    return '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatChipsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
